63b2b6cfea9542100ed76186b5dda343e338f956
Veronika Lapenok <veronika.lapenok.yr@gmail.com>
2023-10-11 13:54:55 +0300

    added models

---
+++ src/main/java/ru/job4j/cinema/model/Film.java
@@ -0,0 +1,119 @@
+package ru.job4j.cinema.model;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class Film {
+    public static final Map<String, String> COLUMN_MAPPING = Map.of(
+            "id", "id",
+            "name", "name",
+            "description", "description",
+            "year", "year",
+            "genre_id", "genreId",
+            "minimal_age", "minAge",
+            "duration_in_minutes", "duration",
+            "file_id", "fileId"
+    );
+
+    private int id;
+    private int name;
+    private String description;
+    private int year;
+    private int genreId;
+    private int minAge;
+    private int duration;
+    private int fileId;
+
+    public Film(int id, int name, String description, int year, int genreId, int minAge,
+                int duration, int fileId) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.year = year;
+        this.genreId = genreId;
+        this.minAge = minAge;
+        this.duration = duration;
+        this.fileId = fileId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getGenreId() {
+        return genreId;
+    }
+
+    public void setGenreId(int genreId) {
+        this.genreId = genreId;
+    }
+
+    public int getMinAge() {
+        return minAge;
+    }
+
+    public void setMinAge(int minAge) {
+        this.minAge = minAge;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(int fileId) {
+        this.fileId = fileId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Film film = (Film) o;
+        return id == film.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
